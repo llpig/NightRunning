af152001424d5003e0ee35f84f6f2f366ec314f1
kongchenrui <596754016@qq.com>
2020-04-17 00:37:08 +0800

    完成注册界面

--- app/src/main/java/com/kong/nightrunning/UserRegisteredActivity.java
+++ app/src/main/java/com/kong/nightrunning/UserRegisteredActivity.java
@@ -4,19 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.io.File;
 
 public class UserRegisteredActivity extends AppCompatActivity {
 
     private Button mButtonReturnLogin;
     private Button mButtonRegistered;
+    private RadioGroup mRadioGroupSex;
+    private ImageView mImageViewUserAvatar;
     private Tool tool;
     private NightRunningDatabase helper;
+    private String mUserAvatarPath = null;
+    private static int USERAVATARPATHREQUESTCODE = 10;
+    private static int USERSEX = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,37 +40,86 @@ public class UserRegisteredActivity extends AppCompatActivity {
     }
 
     public void initActivity() {
-        tool=new Tool();
-        helper=MainActivity.getDatabaseHelper();
+        tool = new Tool();
+        helper = MainActivity.getDatabaseHelper();
         getSupportActionBar().hide();
         findViewAndSetOnClickListener();
-        Log.i("DATA","进入注册界面");
+        Log.i("DATA", "进入注册界面");
     }
 
     private void findViewAndSetOnClickListener() {
         ViewOnClickListener onClickListener = new ViewOnClickListener();
         mButtonReturnLogin = findViewById(R.id.ButtonReturnLogin);
         mButtonReturnLogin.setOnClickListener(onClickListener);
-        mButtonRegistered=findViewById(R.id.ButtonRegistered);
+        mButtonRegistered = findViewById(R.id.ButtonRegistered);
         mButtonRegistered.setOnClickListener(onClickListener);
+        mImageViewUserAvatar = findViewById(R.id.ImageViewRegUserAvatar);
+        mImageViewUserAvatar.setOnClickListener(onClickListener);
+        mRadioGroupSex = findViewById(R.id.sexRadioGroup);
+        mRadioGroupSex.setOnCheckedChangeListener(new OnRadioGroupCheckedChangeListener());
     }
 
-    private void getInputInformationSave(){
-        EditText userName=findViewById(R.id.EditTextRegUserName);
-        EditText password=findViewById(R.id.EditTextRegPassword);
-        String userNameText=userName.getText().toString().trim();
-        String passwordText=tool.getMD5Code(password.getText().toString().trim());
-        SQLiteDatabase db=helper.getReadableDatabase();
+    private void getInputInformationSave() {
+        //用户名
+        String userName = ((EditText) findViewById(R.id.EditTextRegUserName)).getText().toString().trim();
+        //邮箱
+        String email = ((EditText) findViewById(R.id.EditTextRegEmail)).getText().toString().trim();
+        //年龄
+        int age = Integer.parseInt(((EditText) findViewById(R.id.EditTextRegAge)).getText().toString().trim());
+        //身高
+        int height = Integer.parseInt(((EditText) findViewById(R.id.EditTextRegHeight)).getText().toString().trim());
+        //体重
+        double weight = Double.parseDouble(((EditText) findViewById(R.id.EditTextRegWeight)).getText().toString().trim());
+        //目标步数
+        int targetStepNumber = Integer.parseInt(((EditText) findViewById(R.id.EditTextRegTargetStepNumber)).getText().toString().trim());
+        //目标里程
+        double targetMileage = Double.parseDouble(((EditText) findViewById(R.id.EditTextRegTargetMileage)).getText().toString().trim());
+        //密码
+        String password = tool.getMD5Code(((EditText) findViewById(R.id.EditTextRegPassword)).getText().toString().trim());
+        //确认密码
+        String checkPassword = tool.getMD5Code(((EditText) findViewById(R.id.EditTextRegCheckPassword)).getText().toString().trim());
+
+        Log.i("DATA", "用户名:" + userName + ",邮箱:" + email + ",年龄：" + age + ",身高" + height + ",体重:" + weight +
+                "目标步数：" + targetStepNumber + ",目标里程：" + targetMileage + ",密码：" + password + ",确认密码：" + checkPassword + ",头像：" + mUserAvatarPath + ",性别：" + USERSEX);
+        SQLiteDatabase db = helper.getReadableDatabase();
 //        helper.insertRecordsToUserInfoTable(db,userNameText,passwordText);
         //将数据更新到数据库
 
-        try {
-            tool.showToast(this,"注册成功，请登录。");
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        tool.startActivityFromIntent(UserRegisteredActivity.this, UserLoginActivity.class);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == USERAVATARPATHREQUESTCODE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            assert cursor != null;
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            mUserAvatarPath = cursor.getString(columnIndex);
+            mImageViewUserAvatar.setImageURI(Uri.parse(mUserAvatarPath));
+            cursor.close();
+            Log.i("DATA", mUserAvatarPath);
         }
-        tool.startActivityFromIntent(UserRegisteredActivity.this,UserLoginActivity.class);
+    }
+
+    private class OnRadioGroupCheckedChangeListener implements RadioGroup.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId) {
+                case R.id.manRadioButton: {
+                    USERSEX = 0;
+                    break;
+                }
+                case R.id.womenRadioButton: {
+                    USERSEX = 1;
+                    break;
+                }
+            }
+        }
     }
 
     private class ViewOnClickListener implements View.OnClickListener {
@@ -64,13 +127,19 @@ public class UserRegisteredActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.ButtonReturnLogin: {
-                    tool.startActivityFromIntent(UserRegisteredActivity.this,UserLoginActivity.class);
-                    Log.i("DATA","返回登录界面");
+                    tool.startActivityFromIntent(UserRegisteredActivity.this, UserLoginActivity.class);
+                    Log.i("DATA", "返回登录界面");
                     break;
                 }
-                case R.id.ButtonRegistered:{
+                case R.id.ButtonRegistered: {
                     getInputInformationSave();
-                    Log.i("DATA","注册完成");
+                    Log.i("DATA", "注册完成");
+                    break;
+                }
+                case R.id.ImageViewRegUserAvatar: {
+                    //调用系统相册
+                    startActivityForResult(new Intent(
+                            Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), USERAVATARPATHREQUESTCODE);
                     break;
                 }
             }
